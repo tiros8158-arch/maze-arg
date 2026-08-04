e85dbd296443754e981d00bcd43c6fbff7251010
tiros8158-arch <tiros8158@gmail.com>
2026-08-04 15:08:55 +0200

    Update service.js

--- service.js
+++ service.js
@@ -1,1 +1,302 @@
+function menuPrincipal(){
 
+document.getElementById("contenuService").innerHTML=`
+
+<p>Bienvenue sur le service de ####</p>
+
+<button onclick="detruire()">
+1 - Se débarrasser de votre ####
+</button>
+
+<br><br>
+
+<button onclick="informations()">
+2 - Informations
+</button>
+
+<br><br>
+
+<button onclick="bizarre()">
+3 - Il est bizarre
+</button>
+
+<br><br>
+
+<button>
+4 - Service de réparation
+</button>
+
+<br><br>
+
+<button onclick="window.location.href='index.html'">
+Quitter
+</button>
+
+`;
+
+}
+
+
+
+function detruire(){
+
+document.getElementById("contenuService").innerHTML=`
+
+<p><b>Coordonnées GPS trouvées</b></p>
+
+<p>
+Latitude : 45.9750° N<br>
+Longitude : 5.1167° E
+</p>
+
+<p>
+Une machine capable de désintégrer le #### du monde se trouve à cet endroit.
+</p>
+
+<button onclick="menuPrincipal()">
+N - Retour
+</button>
+
+`;
+
+}
+
+
+
+function informations(){
+
+document.getElementById("contenuService").innerHTML=`
+
+<p>Tapez :</p>
+
+<button onclick="comportement()">
+1 - Informations sur le comportement
+</button>
+
+<br><br>
+
+<button onclick="personnalite()">
+2 - Informations sur la personnalité
+</button>
+
+<br><br>
+
+<button onclick="libre()">
+3 - Informations libres
+</button>
+
+<br><br>
+
+<button onclick="menuPrincipal()">
+0 - Retour
+</button>
+
+`;
+
+}
+
+
+
+function comportement(){
+
+document.getElementById("contenuService").innerHTML=`
+
+<p><b>Comportement du ####</b></p>
+
+<p>
+Le #### est très énergique et théâtral.<br><br>
+Il est très très très très très très très énergique.<br><br>
+Il adore la musique.<br><br>
+Il déteste être seul.
+</p>
+
+<button onclick="informations()">
+0 - Retour
+</button>
+
+`;
+
+}
+function personnalite(){
+
+document.getElementById("contenuService").innerHTML=`
+
+<p>
+Veuillez entrer le numéro de série de votre #### :
+</p>
+
+<input id="serie" type="text">
+
+<br><br>
+
+<button onclick="verifierSerie()">
+Valider
+</button>
+
+<br><br>
+
+<button onclick="informations()">
+0 - Retour
+</button>
+
+`;
+
+}
+
+
+
+function verifierSerie(){
+
+let serie = document.getElementById("serie").value;
+
+
+if(serie=="55341"){
+
+document.getElementById("contenuService").innerHTML=`
+
+<p><b>ACCESS AUTORISÉ</b></p>
+
+<p>
+LEAVE THE HUGE MAZE LABORATORY<br>
+OR THE MASTER WILL SETTLE YOUR ACCOUNT
+</p>
+
+<button onclick="informations()">
+0 - Retour
+</button>
+
+`;
+
+}
+
+else{
+
+document.getElementById("contenuService").innerHTML=`
+
+<p>
+NUMÉRO DE SÉRIE INVALIDE
+</p>
+
+<button onclick="personnalite()">
+Réessayer
+</button>
+
+<br><br>
+
+<button onclick="informations()">
+0 - Retour
+</button>
+
+`;
+
+}
+
+}
+
+
+
+function libre(){
+
+document.getElementById("contenuService").innerHTML=`
+
+<p><b>Recherche d'informations libres...</b></p>
+
+<p>
+Connexion aux données...
+</p>
+
+<p>
+ERREUR : INTERNET NON CONNECTÉ
+</p>
+
+<p>
+Impossible d'accéder aux informations libres.
+</p>
+
+<button onclick="informations()">
+0 - Retour
+</button>
+
+`;
+
+}
+function bizarre(){
+
+document.getElementById("contenuService").innerHTML=`
+
+<p>
+Votre #### est bizarre dans :
+</p>
+
+<button onclick="bizarreComportement()">
+1 - Son comportement
+</button>
+
+<br><br>
+
+<button onclick="bizarreCouleur()">
+2 - Sa couleur
+</button>
+
+<br><br>
+
+<button>
+3 - Sa personnalité
+</button>
+
+<br><br>
+
+<button>
+4 - Il veut vous bouffer
+</button>
+
+<br><br>
+
+<button onclick="menuPrincipal()">
+0 - Retour
+</button>
+
+`;
+
+}
+
+
+
+function bizarreComportement(){
+
+document.getElementById("contenuService").innerHTML=`
+
+<p><b>Analyse du comportement</b></p>
+
+<p>
+Essayez de le comprendre et de voir ce qui ne va pas.
+</p>
+
+<button onclick="bizarre()">
+0 - Retour
+</button>
+
+`;
+
+}
+
+
+
+function bizarreCouleur(){
+
+document.getElementById("contenuService").innerHTML=`
+
+<p><b>Analyse de la couleur</b></p>
+
+<p>
+La couleur du #### semble anormale.<br><br>
+Les données indiquent une variation inconnue.
+</p>
+
+<button onclick="bizarre()">
+0 - Retour
+</button>
+
+`;
+
+}
